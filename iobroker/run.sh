ffc4096ce72628e4c3db65d014d6a6447d35fa1f
Edwin ANNE <edwin_anne@outlook.fr>
2025-04-22 10:30:55 +0200

    fix

--- iobroker/run.sh
+++ iobroker/run.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Démarrage de ioBroker..."
+echo "Hello world!"
 
-# Installer ioBroker dans le répertoire de données
-cd /data
+# Installer ioBroker
 npm install iobroker --unsafe-perm
 
 # Démarrer ioBroker
-iobroker start
-
-# Attendre que ioBroker soit prêt
-while ! nc -z localhost 8081; do
-  echo "En attente du démarrage de ioBroker..."
-  sleep 1
-done
-
-echo "ioBroker est démarré et accessible sur le port 8081"
-
-# Maintenir le conteneur en vie et afficher les logs
-tail -f /data/iobroker/log/iobroker.log 
+iobroker start 
